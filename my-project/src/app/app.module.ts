aed9ff9fb41a9220b54b5412fe34ab1bec7fd301
kevin schuchard <schuchard.kevin@gmail.com>
2018-06-11 10:16:39 -0600

    remove test components from example project

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
   ],
   imports: [
     BrowserModule

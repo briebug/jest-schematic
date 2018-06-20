c2ec0ffaee0acca4c17507be2688aa27055901b3
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-06-20 12:05:05 -0600

    feat(test): align with jest testing project
    add jest "tranform" mock

--- src/jest/files/src/setup-jest.ts
+++ src/jest/files/src/setup-jest.ts
@@ -17,5 +17,14 @@ Object.defineProperty(window, 'getComputedStyle', {
   value: () => ['-webkit-appearance'],
 });
 
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true,
+    };
+  },
+});
+
 /* output shorter and more meaningful Zone error stack traces */
 // Error.stackTraceLimit = 2;

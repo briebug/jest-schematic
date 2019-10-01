43b2441459e8ca0fa0bdcbca13f1ffb165eebdd1
Kevin Schuchard <schuchard.kevin@gmail.com>
2019-10-01 08:32:17 -0600

    Angular 8 changes (#17)
    * remove sandbox
    
    * update deps
    
    * new sandbox v8
    
    * pin rxjs for v8 schematics
    
    * remove test.ts from tsconfig.spec.ts
    - fixes #15
    
    * Passing tests in v8
    - move jest config to package.json
    - remove jest.config.js file from schematic
    
    v8 fixes appear to be
    astTransformers, tsconfig.spec
    
    * switch to circleci
    
    * bump node image
    
    * change sandbox build script

--- sandbox/README.md
+++ sandbox/README.md
@@ -1,6 +1,6 @@
 # Sandbox
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.2.0.
 
 ## Development server
 
--- sandbox/src/app/app.module.ts
+++ sandbox/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

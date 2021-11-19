cdc63e7990284a7350a650c59f22e023b3dde505
Patrice de Saint Steban <patrice@desaintsteban.fr>
2021-11-19 15:52:41 +0100

    Update jestconfig generated file

--- src/jest/workspace-files/jest.config.js
+++ src/jest/workspace-files/jest.config.js
@@ -2,9 +2,14 @@ const baseConfig = require('../../jest.config');
 
 module.exports = {
   ...baseConfig,
+  moduleNameMapper: {
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+  },
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globals: {
     'ts-jest': {
-      tsConfig: '<rootDir>/<%= path %>/tsconfig.spec.json',
+      tsconfig: '<rootDir>/<%= path %>/tsconfig.spec.json',
     },
   },
 };
